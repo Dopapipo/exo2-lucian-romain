e3f717f053cb7c5b672a16f599ce378882fd36b1
Romain <romainjacob012@gmail.com>
2024-12-04 12:16:34 +0100

    Footer creation

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  template: `
+    <footer class="fixed-bottom bg-light p-3 gx-2 text-end">
+      <button class="btn btn-primary" (click)="goToContact()">Contact</button>
+    </footer>
+  `
 })
 export class FooterComponent {
+  constructor(private router: Router) {}
 
+  goToContact() {
+    this.router.navigate(['/contact']);
+  }
 }
