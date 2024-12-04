42bfc5fa60492d7ca4022a35d0a74eeecc13e168
Lucian <lucian@jducraft.com>
2024-12-04 12:10:06 +0100

    routage et header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import {MoviePageComponent} from "./movie-page/movie-page.component";
+import {GestionPageComponent} from "./gestion-page/gestion-page.component";
+import {HomePageComponent} from "./home-page/home-page.component";
+export const routes: Routes = [
+  { path: '', redirectTo: '/accueil', pathMatch: 'full' },
+  { path: 'accueil', component: HomePageComponent },
+  { path: 'liste', component: MoviePageComponent },
+  { path: 'gestion', component: GestionPageComponent },
+  { path: '**', redirectTo: '/accueil' }
+];
